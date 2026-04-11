483f050a1618a19e17459b93a3ebfb07af053ade
storyncode <daniel@storyncode.com>
2026-04-11 23:03:32 +0100

    chore(05-02): refresh inline customization guidance
    - describe init.lua as a thin entrypoint into story.bootstrap
    - point custom plugins guidance at the modular README and ownership boundaries

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 --[[
 
 =====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
+====================== START HERE IF YOU'RE CUSTOMIZING ======================
 =====================================================================
 ========                                    .-----.          ========
 ========         .----------------------.   | === |          ========
 ========         |.-""""""""""""""""""-.|   |-----|          ========
 ========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
+========         ||     STORYNVIM      ||   |-----|          ========
 ========         ||                    ||   | === |          ========
 ========         ||                    ||   |-----|          ========
 ========         ||:Tutor              ||   |:::::|          ========
@@ -20,68 +20,33 @@
 =====================================================================
 =====================================================================
 
-What is Kickstart?
+This file is intentionally small.
 
-  Kickstart.nvim is *not* a distribution.
+  `init.lua` only handles early globals and then hands off startup to:
+    `require('story.bootstrap').setup()`
 
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
+Where to change things:
 
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
+  - `lua/core/` for always-on editor behavior like options, keymaps,
+    diagnostics, and autocmds
+  - `lua/plugins/` for shipped plugin specs and plugin configuration
+  - `lua/custom/plugins/` for your own plugins, overrides, and fork-local changes
 
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
+Compatibility note:
 
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
+  - `lua/kickstart/plugins/` still exists as a compatibility/example namespace,
+    but it is not the preferred path for new customization in this fork.
 
-Kickstart Guide:
+Useful references:
 
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
+  - `README.md` for the current architecture and customization guide
+  - `:help kickstart.nvim` for the shipped in-editor overview
+  - `:checkhealth kickstart` if installation or environment checks fail
+  - `:help lua-guide` for Neovim's Lua integration docs
+  - `:Tutor` if you are still learning core Neovim motions and editing
 
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
+Edit the module that matches the change you want instead of treating this file
+as the main configuration surface.
 --]]
 
 -- Set <space> as the leader key
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,7 +1,9 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
--- See the kickstart.nvim README for more information
+-- Start with README.md for the current architecture guide.
+-- `lua/custom/plugins/` is the stable place for your fork-local plugin specs,
+-- overrides, and experiments without editing the shipped `lua/plugins/` modules.
 
 ---@module 'lazy'
 ---@type LazySpec
