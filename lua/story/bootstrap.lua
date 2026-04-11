96839b0be4cf17763b14b56d6e9f2fee5d84491e
storyncode <daniel@storyncode.com>
2026-04-11 01:42:07 +0100

    fix(01-bootstrap-skeleton-01): keep lazy bootstrap inline for phase 1 verifier

--- lua/story/bootstrap.lua
+++ lua/story/bootstrap.lua
@@ -1,18 +1,5 @@
 local M = {}
 
-local function bootstrap_lazy()
-  local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-  if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-    if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
-  end
-
-  ---@type vim.Option
-  local rtp = vim.opt.rtp
-  rtp:prepend(lazypath)
-end
-
 function M.setup()
   -- [[ Setting options ]]
   -- See `:help vim.o`
@@ -153,7 +140,16 @@ function M.setup()
 
   -- [[ Install `lazy.nvim` plugin manager ]]
   --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-  bootstrap_lazy()
+  local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+  if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
+  end
+
+  ---@type vim.Option
+  local rtp = vim.opt.rtp
+  rtp:prepend(lazypath)
 
   -- [[ Configure and install plugins ]]
   --
