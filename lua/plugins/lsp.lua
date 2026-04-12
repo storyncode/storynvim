ad125f326e1e4f9aa0dcf6628e895d616cd42e63
storyncode <daniel@storyncode.com>
2026-04-12 21:52:53 +0100

    update: add marksman

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -132,6 +132,8 @@ return {
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
 
+        marksman = {},
+
         stylua = {}, -- Used to format Lua code
 
         -- Special Lua Config, as recommended by neovim help docs
