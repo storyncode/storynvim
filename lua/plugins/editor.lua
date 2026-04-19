a6610631fe688392b5d0d7632cfce1ffed64818a
storyncode <daniel@storyncode.com>
2026-04-19 19:51:11 +0100

    update: add obdisian.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -68,4 +68,28 @@ return {
       },
     },
   },
+  {
+
+    'obsidian-nvim/obsidian.nvim',
+    version = '*', -- use latest release, remove to use latest commit
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+      legacy_commands = false, -- this will be removed in the next major release
+      workspaces = {
+        {
+          name = 'personal',
+          path = '~/vaults/personal',
+        },
+        {
+          name = 'work',
+          path = '~/vaults/work',
+        },
+        {
+          name = 'pop',
+          path = '~/vaults/priceofpower',
+        },
+      },
+    },
+  },
 }
